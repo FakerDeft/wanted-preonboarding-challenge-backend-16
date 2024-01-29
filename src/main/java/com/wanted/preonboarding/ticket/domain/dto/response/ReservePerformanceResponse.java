3186a20c4e244a9ee9100f24a1a0ca80ee657045
fakerdeft <whakswp123@naver.com>
2024-01-29 15:03:29 +0900

    refactor: 예약 결과 응답 객체 추가

---
+++ src/main/java/com/wanted/preonboarding/ticket/domain/dto/response/ReservePerformanceResponse.java
@@ -0,0 +1,33 @@
+package com.wanted.preonboarding.ticket.domain.dto.response;
+
+import com.wanted.preonboarding.ticket.domain.entity.Performance;
+import com.wanted.preonboarding.ticket.domain.entity.Reservation;
+
+import java.util.UUID;
+
+public record ReservePerformanceResponse(
+        UUID performanceId, // 공연 아이디
+        String memberName, // 예약자 이름
+        String memberPhoneNumber, // 예약자 폰 번호
+        String performanceName, // 공연명
+        int round, // 공연 회차
+        int gate, // 좌석 정보
+        char line, // 좌석 정보
+        int seat // 좌석 정보
+) {
+    public static ReservePerformanceResponse of(
+            final Reservation reservationInfo,
+            final Performance performanceInfo
+    ) {
+        return new ReservePerformanceResponse(
+                performanceInfo.getPerformanceId(),
+                reservationInfo.getMemberName(),
+                reservationInfo.getMemberPhoneNumber(),
+                performanceInfo.getPerformanceName(),
+                performanceInfo.getRound(),
+                reservationInfo.getGate(),
+                reservationInfo.getLine(),
+                reservationInfo.getSeat()
+        );
+    }
+}
