6533acb7cbfcd382403f118ee7e390ab67e9b2fc
fakerdeft <whakswp123@naver.com>
2024-01-29 14:11:11 +0900

    feat: 예약 예외 추가

--- src/main/java/com/wanted/preonboarding/ticket/exception/TicketException.java
+++ src/main/java/com/wanted/preonboarding/ticket/exception/TicketException.java
@@ -8,6 +8,18 @@ public class TicketException extends RuntimeException {
         super(message);
     }
 
+    public static class ReservationNotFoundException extends TicketException {
+
+        public ReservationNotFoundException(
+                final String memberName,
+                final String memberPhoneNumber
+        ) {
+            super(String.format("조회하는 예약이 존재하지 않습니다. - request info { member_name : %s, member_phone_number : %s }",
+                    memberName,
+                    memberPhoneNumber));
+        }
+    }
+
     public static class PerformanceNotFoundException extends TicketException {
 
         public PerformanceNotFoundException(final UUID performanceId) {
