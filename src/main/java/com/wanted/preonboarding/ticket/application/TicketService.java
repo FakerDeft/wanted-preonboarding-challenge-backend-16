a0de457703a9a365a7f8498a18d753f66345df4a
fakerdeft <whakswp123@naver.com>
2024-01-29 14:56:03 +0900

    refactor: 예약 조회 응답 내용 수정

--- src/main/java/com/wanted/preonboarding/ticket/application/TicketService.java
+++ src/main/java/com/wanted/preonboarding/ticket/application/TicketService.java
@@ -1,8 +1,9 @@
 package com.wanted.preonboarding.ticket.application;
 
-import com.wanted.preonboarding.ticket.domain.dto.PerformanceDTO;
+import com.wanted.preonboarding.ticket.domain.dto.request.FindPerformanceRequest;
 import com.wanted.preonboarding.ticket.domain.dto.request.FindReservationRequest;
 import com.wanted.preonboarding.ticket.domain.dto.request.ReservePerformanceRequest;
+import com.wanted.preonboarding.ticket.domain.dto.response.FindReservationResponse;
 import com.wanted.preonboarding.ticket.domain.entity.Performance;
 import com.wanted.preonboarding.ticket.domain.entity.Reservation;
 import com.wanted.preonboarding.ticket.exception.TicketException;
@@ -22,27 +23,32 @@ public class TicketService {
     private final ReservationRepository reservationRepository;
     private final long totalAmount = 0L;
 
-    public List<PerformanceDTO> getAllPerformanceInfoList() {
-        return performanceRepository.findByIsReserve("enable")
-                .stream()
-                .map(PerformanceDTO::of)
-                .toList();
-    }
-
-    public PerformanceDTO getPerformanceInfoDetail(String name) {
-        return PerformanceDTO.of(performanceRepository.findByName(name));
-    }
-
     /**
      * [예약 조회 메서드]
      * 회원의 이름과 핸드폰 번호로 예약 조회
-     * 예약 조회 후 결과 리턴
+     * 해당 예약의 공연 id로 공연 조회
+     * 조회 후 결과 리턴
      */
-    public Reservation findReservation(final FindReservationRequest findReservationRequest) {
-        return reservationRepository.findByMemberNameAndMemberPhoneNumber(
+    public FindReservationResponse findReservation(final FindReservationRequest findReservationRequest) {
+
+        Reservation reservation = reservationRepository.findByMemberNameAndMemberPhoneNumber(
                         findReservationRequest.memberName(),
                         findReservationRequest.memberPhoneNumber())
                 .orElseThrow(() -> new TicketException.ReservationNotFoundException(findReservationRequest.memberName(), findReservationRequest.memberPhoneNumber()));
+        Performance performance = performanceRepository.findById(reservation.getPerformanceId())
+                .orElseThrow(() -> new TicketException.PerformanceNotFoundException(reservation.getPerformanceId()));
+
+        return FindReservationResponse.of(reservation, performance);
+    }
+
+    /**
+     * [예매 가능한 공연 리스트 조회 메서드]
+     * 예매 가능 여부('enable')로 조회
+     * 조회 후 결과 리스트 리턴
+     */
+    public List<Performance> getAllPerformanceInfoList(final FindPerformanceRequest findPerformanceRequest) {
+        return performanceRepository.findByIsReserve(findPerformanceRequest.isReserve())
+                .orElseThrow(() -> new TicketException.EnablePerformanceNotFoundException(findPerformanceRequest.isReserve()));
     }
 
     /**
@@ -71,6 +77,10 @@ public class TicketService {
         return null;
     }
 
+    /**
+     * [최종 금액 계산 메서드]
+     * 최종 금액 = 잔고 - 공연 가격 - 할인 금액
+     */
     private long calculateReservationPrice(
             final long amount,
             final long performancePrice,
